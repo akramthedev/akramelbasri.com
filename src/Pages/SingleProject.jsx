e8173843abff6217cf285497c0aa30348db91a84
akramthedev <162736569+akramthedev@users.noreply.github.com>
2024-05-18 13:39:27 +0100

    Update SingleProject.jsx

--- src/Pages/SingleProject.jsx
+++ src/Pages/SingleProject.jsx
@@ -271,7 +271,7 @@ const SingleProject = () => {
           ID && ID === 4  && 
           <>
             <>
-              <a href="https://drive.google.com/drive/folders/1Y1dWf9SI32ptnSy-cCvyD3zOl99cn1Xx" target="_blank" >Code Source</a>
+              <a style={{color : "whitesmoke"}}  href="https://drive.google.com/drive/folders/1Y1dWf9SI32ptnSy-cCvyD3zOl99cn1Xx" target="_blank" >Code Source</a>
               <br/>
               <a style={{color : "whitesmoke"}}  href="https://drive.google.com/drive/folders/1pJWKAmm4IENeN1RGUGR7hYRlqQhIJ0yA" target="_blank" >SRS & Presentation</a>
             </>
