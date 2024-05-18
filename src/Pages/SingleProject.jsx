9e2cefd2e35cfe2c5097e8e4d448243215605ad8
akramthedev <162736569+akramthedev@users.noreply.github.com>
2024-05-18 13:35:44 +0100

    Update SingleProject.jsx

--- src/Pages/SingleProject.jsx
+++ src/Pages/SingleProject.jsx
@@ -273,7 +273,7 @@ const SingleProject = () => {
             <>
               <a href="https://drive.google.com/drive/folders/1Y1dWf9SI32ptnSy-cCvyD3zOl99cn1Xx" target="_blank" >Code Source</a>
               <br/>
-              <a href="https://drive.google.com/drive/folders/1pJWKAmm4IENeN1RGUGR7hYRlqQhIJ0yA" target="_blank" >SRS & Presentation</a>
+              <a style={{color : "whitesmoke"}}  href="https://drive.google.com/drive/folders/1pJWKAmm4IENeN1RGUGR7hYRlqQhIJ0yA" target="_blank" >SRS & Presentation</a>
             </>
             <br/>
             <div className="rowzuididf">
