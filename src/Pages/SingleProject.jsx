5bdee72219238e4ecd1662837e8b55c9c8a5a235
akramthedev <162736569+akramthedev@users.noreply.github.com>
2024-05-26 03:40:38 +0100

    Update SingleProject.jsx

--- src/Pages/SingleProject.jsx
+++ src/Pages/SingleProject.jsx
@@ -271,9 +271,11 @@ const SingleProject = () => {
           ID && ID === 4  && 
           <>
             <>
-              <a style={{color : "whitesmoke"}}  href="https://drive.google.com/drive/folders/1Y1dWf9SI32ptnSy-cCvyD3zOl99cn1Xx" target="_blank" >Code Source</a>
+              <a style={{color : "whitesmoke"}}  href="https://drive.google.com/drive/folders/1Y1dWf9SI32ptnSy-cCvyD3zOl99cn1Xx" target="_blank" >Code Source Drive </a>
               <br/>
               <a style={{color : "whitesmoke"}}  href="https://drive.google.com/drive/folders/1pJWKAmm4IENeN1RGUGR7hYRlqQhIJ0yA" target="_blank" >SRS & Presentation</a>
+              <br/>
+              <a style={{color : "whitesmoke"}}  href="https://github.com/akramthedev/APPFE" target="_blank" >Code source GitHub</a>
             </>
             <br/>
             <div className="rowzuididf">
